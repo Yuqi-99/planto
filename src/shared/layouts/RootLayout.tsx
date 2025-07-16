6f18dc0dbc394aef550a2abfdb60fb6eab3e7729
yuqi.ong <yuqi.ong@atoz-software.tech>
2025-07-16 16:31:53 +0800

    feat(dev): add scroll to top func when pathname change

--- src/shared/layouts/RootLayout.tsx
+++ src/shared/layouts/RootLayout.tsx
@@ -1,10 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { ScrollToTop } from '../components/ScrollToTop';
 import { Toast } from '../components/Toast';
 import { Footer } from './Footer';
 import { Header } from './Header';
+import { useEffect } from 'react';
 
 export const RootLayout = () => {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo({
+			top: 0,
+		});
+	}, [pathname]);
+
 	return (
 		<main className='flex items-center justify-center'>
 			<div id='content-wrapper' className='relative min-h-screen w-screen'>
